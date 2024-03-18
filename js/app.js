cb8af1656e889f813cf6bdd0a4d550f1153fb356
giusmili <giusmili67@gmail.com>
2024-03-18 14:33:42 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", e => {
+
     console.log(e.target.children);
     const el = document.querySelector("h1");
     console.log(el.innerText);
     el.innerText = "EcmaScript version avancée";
-    
 });
