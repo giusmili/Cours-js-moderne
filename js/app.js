dfcee82f405456f2961319890469049a99f956ae
giusmili <giusmili67@gmail.com>
2024-03-31 12:04:08 +0200

    css

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { User, options } from "./modules_js.js";
+import { User, options, tab } from "./modules_js.js";
 
 document.addEventListener("DOMContentLoaded", e => {
   //fonction main js
@@ -9,6 +9,30 @@ document.addEventListener("DOMContentLoaded", e => {
   /* 
         const let 
      */
+  const element_h1 = document.querySelector("h1");
+  const element_h2 = document.querySelector("h2");
+  //console.log(element_h1.innerText)
+  element_h1.addEventListener("click", e => {
+
+    //sweet Alert
+    Swal.fire({
+      title: "🚀Bon travail",
+      text: "Tu as bien cliqué ici",
+      icon: "success"
+    });
+  });
+  element_h2.addEventListener("click", () => {
+
+    //sweet Alert
+    Swal.fire({
+      title: "Portfolio",
+      text: "Bonjour chez moi",
+      imageUrl: "https://picsum.photos/seed/picsum/600/200",
+      imageWidth: 600,
+      imageHeight: 200,
+      imageAlt: "Cover image"
+    });
+  });
 
   console.log(typeof User);
   User.user_print();
@@ -29,15 +53,38 @@ document.addEventListener("DOMContentLoaded", e => {
   <
   ||
   &&
+  ? chainage optionnel
   */
-  let nombre = Number(9);
+  let nombre = Number(10);
   let nombre_1 = String("9");
-  const version = 9.5;
+  //const version = parseFloat(prompt("saisir un chiffre",""))
   nombre === nombre_1 ? console.log(true) : console.log(false);
   console.log(nombre * 2);
+
+  nombre % 3 === 1 ? console.log("chiffre paire") : console.log("chiffre impaire");
+  //console.log(version.toFixed(2))
 
   /* Chaînage optionnel en JavaScript {}?. */
   /* npm install --save-dev @babel/plugin-proposal-optional-chaining */
   // Définition de l'objet person avec une propriété address pouvant être nulle
 
+  /* let i = 0;
+   while(i < tab.length){
+      
+      console.log(tab[i])
+      i++; 
+      
+   } */
+  /* for(let i = 0; i < tab.length; i++){
+      console.log((i+1)+" : "+tab[i]+"\n")
+  } */
+
+  /* for(let index in tab){
+   console.log(index+" "+tab[index])
+  }
+  */
+  tab.map((index, element) => {
+    //clé et valeur
+    console.log(element + " " + index);
+  });
 });
--- js/modules_js.js
+++ js/modules_js.js
@@ -1,20 +1,44 @@
 export const User = {
-     user_print() {
-          let el = document.querySelector("h1");
-          // attention pour une chaine de caractères qui contien du code c'est innnerHTML
-          el.innerHTML = '<span aria-hidden="true">🚀</span>Cours javascript moderne version 8';
-          let footer = document.querySelector("footer p");
-          let dates = new Date().getFullYear();
-          let time = new Date().toLocaleDateString("fr-FR", options);
-          const element_time = document.getElementsByTagName("time")[0];
-          console.log(element_time);
-          element_time.setAttribute("datetime", time);
-          footer.innerText += dates;
-     }
+    user_print() {
+        let el = document.querySelector("h1");
+        // attention pour une chaine de caractères qui contien du code c'est innnerHTML
+        el.innerHTML = '<span aria-hidden="true">🚀</span>Cours javascript moderne version 8';
+        let footer = document.querySelector("footer p");
+        let dates = new Date().getFullYear();
+        let time = new Date().toLocaleDateString("fr-FR", options);
+        const element_time = document.getElementsByTagName("time")[0];
+        console.log(element_time);
+        element_time.setAttribute("datetime", time);
+        footer.innerText += dates;
+    }
 };
 export const options = {
-     weekday: "long",
-     year: "numeric",
-     month: "long",
-     day: "numeric"
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+
+    /* tableau */
+};export const tab = ["html", "css", true, false];
+
+export const Users = {
+    nom: "Doe",
+    prenom: "John",
+    ville: "NY",
+    age: 25
 };
+export const Country = {
+    Drapeau: "https://urlr.me/W49Py",
+    Déscription: "La France, pays de l'Europe occidentale...",
+    Capitale: "Paris",
+    Gouvernement: "État unitaire, Régime semi-présidentiel",
+    Population: "67,75 millions (2021) Banque mondiale",
+    Superficie: "551 695 km²",
+    Produit_Intérieur_Brut: "2,958 billions USD (2021) Banque mondiale",
+    Indicatif_téléphonique: "+33",
+    Salaire_minimum: "1 539,42 EUR par mois (juin 2020) Eurostat"
+
+    //console.table(Country)
+};Object.keys(Country).forEach(props => {
+    console.log(props + ': ' + Country[props]);
+});
